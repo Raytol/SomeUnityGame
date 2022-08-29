0f35031e5029b45eb7449ac5f1d5fd3b21c18b80
JEsteral <jesteral.work@gmail.com>
2022-08-29 16:47:14 +0300

    13 monster end

--- SomeUnityGame/Assets/Scripts/mosnter brain/test4.cs
+++ SomeUnityGame/Assets/Scripts/mosnter brain/test4.cs
@@ -6,6 +6,9 @@ using UnityEngine.AI;
 
 public class test4 : MonoBehaviour
 {
+    Animator animator;
+    float X = 0f;
+    float Y = -1f;
 
     public float M;
 
@@ -24,6 +27,8 @@ public class test4 : MonoBehaviour
 
     void Start()
     {
+
+        animator = GetComponent<Animator>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.updateRotation = false;
         rotationSpeed = _navMeshAgent.angularSpeed;
@@ -31,17 +36,18 @@ public class test4 : MonoBehaviour
         InvokeRepeating(nameof(AImove), changePosTime, changePosTime);
         M = 1f;
     }
-
-
     void Update()
     {
-      
+
         float distanceToPlayer = Vector3.Distance(Player.transform.position, _navMeshAgent.transform.position);
-        
+
         switch (M)
         {
             case 1f:
                 print("case 1");
+                animator.SetFloat("x", X);
+                animator.SetFloat("y", Y);
+                animaW();
                 if (distanceToPlayer <= DetectionDistance || IsInView())
                 {
                     M = 2f;
@@ -50,6 +56,9 @@ public class test4 : MonoBehaviour
 
             case 2f:
                 print("case 2");
+                animator.SetFloat("x", X);
+                animator.SetFloat("y", Y);
+                animaR();
                 if (distanceToPlayer <= DetectionDistance || IsInView())
                 {
                     LastPos = Player.position;
@@ -66,12 +75,19 @@ public class test4 : MonoBehaviour
                 }
                 else
                 {
-                  M = 3f;
+                    M = 3f;
                 }
                 break;
 
             case 3f:
                 print("case 3");
+                animator.SetFloat("x", X);
+                animator.SetFloat("y", Y);
+                animaW();
+                if (distanceToPlayer <= DetectionDistance || IsInView())
+                {
+                    M = 2f;
+                }
                 float distanceToLastPos = Vector3.Distance(LastPos, _navMeshAgent.transform.position);
                 _navMeshAgent.SetDestination(LastPos);
                 if (distanceToLastPos <= 5f)
@@ -82,11 +98,13 @@ public class test4 : MonoBehaviour
 
             case 4f:
                 print("GAME END");
-                M = 1f;
+                animator.SetFloat("x", X);
+                animator.SetFloat("y", Y);
+                animaS();
+                _navMeshAgent.SetDestination(AgentBobby.position);
                 break;
         }
     }
-
     Vector3 RandomNavSphere(float distance)
     {
         Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
@@ -95,12 +113,10 @@ public class test4 : MonoBehaviour
         NavMesh.SamplePosition(randomDirection, out navHit, distance, -1);
         return navHit.position;
     }
-
     private void AImove()
     {
         _navMeshAgent.SetDestination(RandomNavSphere(MoveDistance));
     }
-
     private bool IsInView()
     {
         float realAngle = Vector3.Angle(AgentBobby.forward, Player.position - AgentBobby.position);
@@ -109,13 +125,12 @@ public class test4 : MonoBehaviour
         {
             if (realAngle < ViewAngle / 2f && Vector3.Distance(AgentBobby.position, Player.position) <= ViewDistance && hit.transform == Player.transform)
             {
-                
+
                 return true;
             }
         }
         return false;
     }
-
     private void RotateToTarget()
     {
         Vector3 lookVector = Player.position - agentTransform.position;
@@ -133,6 +148,64 @@ public class test4 : MonoBehaviour
         if (IsInView())
         {
             _navMeshAgent.SetDestination(Player.position);
+        }
+    }
+
+    public void animaR()
+    {
+        if (Y <= 1)
+        {
+            Y = Y + 0.01f;
+        }
+        else if (Y >= 1f)
+        {
+            _navMeshAgent.speed = 100f;
+            Y = Y - 0.01f;
+        }
+        if (_navMeshAgent.speed <= 7f)
+        {
+            _navMeshAgent.speed = 7f;
+        }
+        else if (_navMeshAgent.speed >= 7f)
+        {
+            _navMeshAgent.speed = 7f;
+        }
+    }
+    public void animaW()
+    {
+        if (Y <= 0)
+        {
+            _navMeshAgent.speed = 100f;
+            Y = Y + 0.01f;
+        }
+        else if (Y >= 0f)
+        {
+            _navMeshAgent.speed = 100f;
+            Y = Y - 0.01f;
+        }
+        if (_navMeshAgent.speed <= 3.5f)
+        {
+            _navMeshAgent.speed = 3.5f;
+        }
+        else if (_navMeshAgent.speed >= 3.5f)
+        {
+            _navMeshAgent.speed = 7f;
+        }
+    }
+    public void animaS()
+    {
+        if (Y >= -1)
+        {
+            _navMeshAgent.speed = 100f;
+            Y = Y - 0.03f;
+        }
+        if (_navMeshAgent.speed <= 0f)
+        {
+            _navMeshAgent.speed = 0f;
+        }
+        else if (_navMeshAgent.speed >= 0f)
+        {
+            _navMeshAgent.speed = 7f;
         }
     }
 }
