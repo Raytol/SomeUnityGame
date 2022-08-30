7456fe8ff84d56a3753d25448508d7476a93acd6
Raytol <obbrick@mail.ru>
2022-08-30 11:11:31 +0300

    Update TimeManager.cs

--- SomeUnityGame/Assets/Scripts/TimeManager.cs
+++ SomeUnityGame/Assets/Scripts/TimeManager.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int second, minute1, minute2, hour1, hour2;
+    private float gameTime;
+    private Text myText;
+
     void Start()
     {
-        
+        myText = GetComponent<Text>();    
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        myText.text = hour1 + hour2 + ":" + minute1 + minute2;
+        gameTime += 1 * Time.deltaTime;
+        if (gameTime > 1)
+        {
+            second += 1;
+            gameTime = 0;
+        }
+
+        if (second == 5)
+        {
+            minute2 += 5;
+            second = 0;
+            if (minute2 == 10)
+            {
+                minute1 += 1;
+                minute2 = 0;
+                if (minute1 == 6)
+                {
+                    hour1 += 1;
+                    minute1 = 0;
+                }
+            }
+        }
     }
 }
