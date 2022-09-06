b69bd762ca3db36fa948852d41d06bb213f5ca94
Raytol <obbrick@mail.ru>
2022-09-06 13:57:25 +0300

    NonDesing Loading screen added

--- SomeUnityGame/Assets/Scripts/MenuManager.cs
+++ SomeUnityGame/Assets/Scripts/MenuManager.cs
@@ -2,16 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject Question;
+    public GameObject LoadingScreen;
+    public Slider Bar;
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        LoadingScreen.SetActive(true);
+        //SceneManager.LoadScene(1);
+        StartCoroutine(LoadAsync());
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            Bar.value = asyncLoad.progress;
+
+            if (asyncLoad.progress >= .9f && !asyncLoad.allowSceneActivation)
+            {
+                Question.SetActive(true);
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
     }
 }
