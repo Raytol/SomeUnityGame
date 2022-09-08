b243aefb3f3a2ef6356c84499a1f89a803f72b27
JEsteral <jesteral.work@gmail.com>
2022-09-08 16:09:45 +0300

    38

--- SomeUnityGame/Assets/Scripts/Player_Move.cs
+++ SomeUnityGame/Assets/Scripts/Player_Move.cs
@@ -67,18 +67,8 @@ public class Player_Move : MonoBehaviour
         {
             move_Direction = new Vector3(x_Move, 0f, z_Move);
             move_Direction = transform.TransformDirection(move_Direction);
-
-            if (Input.GetKey(KeyCode.Space))
-            {
-                move_Direction.y += jump;
-            }
             if (Input.GetKey(KeyCode.LeftControl))
             {
-                crunched = true;
-                animator.SetBool("crouche", true);
-                player.height = 1f;
-                speed_Current = speed_Seat;
-                Cam.transform.position = Seat.transform.position;
             }
             else
             { 
